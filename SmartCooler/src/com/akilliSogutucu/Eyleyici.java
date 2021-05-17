bf19bf9c676fdf5aabb9fe2b599e518adcac9519
Yunzeus <54690790+Yunzeus@users.noreply.github.com>
2021-05-17 19:01:53 +0300

    Create Eyleyici java files

---
+++ SmartCooler/src/com/akilliSogutucu/Eyleyici.java
@@ -0,0 +1,13 @@
+package com.smartCooler;
+
+public class Eyleyici implements IEyleyici{
+    @Override
+    public void turnCooler() {
+
+    }
+
+    @Override
+    public void closeCooler() {
+
+    }
+}
---
+++ SmartCooler/src/com/akilliSogutucu/IEyleyici.java
@@ -0,0 +1,6 @@
+package com.smartCooler;
+
+public interface IEyleyici {
+    public void turnCooler();
+    public void closeCooler();
+}
